67ab03c9e47a7b370f271e73eceed893adb76811
Steph <steph.d3v@pm.me>
2024-12-06 23:47:32 +0400

    Day 5

--- Readme.md
+++ Readme.md
@@ -8,7 +8,7 @@ You can view all my solutions in Python by clicking on the calendar below:
 
 |    Su    |    Mo    |    Tu    |    We    |    Th    |    Fr    |    Sa    |
 |:--------:|:--------:|:--------:|:--------:|:--------:|:--------:|:--------:|
-|  [1][1]  |  [2][2]  |  [3][3]  |  [4][4]  |     5    |     6    |     7    |
+|  [1][1]  |  [2][2]  |  [3][3]  |  [4][4]  |  [5][5]  |     6    |     7    |
 |     8    |     9    |    10    |    11    |    12    |    13    |    14    |
 |    15    |    16    |    17    |    18    |    19    |    20    |    21    |
 |    22    |    23    |    24    |    25    |    26    |    27    |    28    |
@@ -24,3 +24,4 @@ _Input files are not shared here to preserve [Eric Wastl][was]'s copyright._
 [2]:  src/02.py
 [3]:  src/03.py
 [4]:  src/04.py
+[5]:  src/05.py
---
+++ src/05.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+from functools import cmp_to_key
+from sys import argv
+
+with open(argv[1]) as file:
+    content = file.read()
+
+part1, part2 = content.split("\n\n")
+
+rules = (tuple(map(int, line.split("|"))) for line in part1.splitlines())
+updates = (list(map(int, line.split(","))) for line in part2.splitlines())
+
+after = defaultdict(list)
+for a, b in rules:
+    after[a].append(b)
+
+new_updates = [[], []] # to be sorted, already sorted
+
+def is_sorted(update):
+    for i, a in enumerate(update):
+        for b in update[i+1:]:
+            if a in after[b]:
+                return False
+    return True
+
+for update in updates:
+    new_updates[s := is_sorted(update)].append(
+        update if s else sorted(
+            update,
+            key=cmp_to_key(lambda a, b: -1 if b in after[a] else 1)
+        )
+    )
+
+n2, n1 = map(lambda updated: sum(u[len(u)//2] for u in updated), new_updates)
+
+print(n1, n2)
